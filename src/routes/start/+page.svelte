dc328abd511ebc9eb6d37b77831e91a4f09eb1e5
westsea <westsea314@gmail.com>
2023-05-31 13:07:34 +0900

    console test

--- src/routes/start/+page.svelte
+++ src/routes/start/+page.svelte
@@ -1,8 +1,15 @@
 <script>
+
   let photoURL = null;
+  let test = null;
+  let file = null;
+  let fileInput = null;
+  $: photoURL, test, file;
 
   function handlePhotoCapture(event) {
-    const file = event.target.files[0];
+    console.log(fileInput);
+    file = fileInput.current.files[0]
+    console.log(file);
     
     if (file) {
       const reader = new FileReader();
@@ -18,13 +25,19 @@
   {#if photoURL}
     <img src={photoURL} alt="Captured Photo" />
   {:else}
-    <input type="file" accept="image/*" capture="camera" onchange={handlePhotoCapture} />
+    <input type="file" accept="image/*" capture="camera" onchange={handlePhotoCapture} bind:this={fileInput}/>
+    <div id="test">test</div>
   {/if}
+  <div>{photoURL}</div>
+  <div>{file}</div>
+  <div>{test}</div>
+  <div>{fileInput}</div>
 </main>
 
 <style>
   main {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
@@ -33,5 +46,9 @@
   img {
     max-width: 100%;
     max-height: 100%;
+  }
+  #test {
+    width: 10px;
+    background-color: #cdcdcd;
   }
 </style>
